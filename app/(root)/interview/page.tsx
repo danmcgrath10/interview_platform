9fc3027ea285d738785783d2af5e1b18f52e53a7
Danny McGrath <danmcgrath1035@gmail.com>
2025-05-16 16:08:50 -0400

    added interview generation page

---
+++ app/(root)/interview/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Agent from "@/components/Agent";
+
+const Page = () => {
+    return (
+        <div>
+            <Agent username={"You"} userId={"user1"} type={"generate"} />
+        </div>
+    )
+}
+export default Page
